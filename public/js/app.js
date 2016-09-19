de63949d3e5936ed37dd25a4ac693e3f8d1c1a82
liudonghua123 <liudonghua123@gmail.com>
2016-09-19 09:17:18 +0800

    add export excel feature

--- public/js/app.js
+++ public/js/app.js
@@ -70,6 +70,76 @@ app.controller('tableCtrl', ['$scope', '$rootScope', '$http', '$filter', 'NgTabl
             });
     };
 
+    function datenum(v, date1904) {
+        if(date1904) v+=1462;
+        var epoch = Date.parse(v);
+        return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
+    }
+
+    function sheet_from_array_of_arrays(data, opts) {
+        var ws = {};
+        var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
+        for(var R = 0; R != data.length; ++R) {
+            for(var C = 0; C != data[R].length; ++C) {
+                if(range.s.r > R) range.s.r = R;
+                if(range.s.c > C) range.s.c = C;
+                if(range.e.r < R) range.e.r = R;
+                if(range.e.c < C) range.e.c = C;
+                var cell = {v: data[R][C] };
+                if(cell.v == null) continue;
+                var cell_ref = XLSX.utils.encode_cell({c:C,r:R});
+
+                if(typeof cell.v === 'number') cell.t = 'n';
+                else if(typeof cell.v === 'boolean') cell.t = 'b';
+                else if(cell.v instanceof Date) {
+                    cell.t = 'n'; cell.z = XLSX.SSF._table[14];
+                    cell.v = datenum(cell.v);
+                }
+                else cell.t = 's';
+
+                ws[cell_ref] = cell;
+            }
+        }
+        if(range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
+        return ws;
+    }
+
+    function Workbook() {
+        if(!(this instanceof Workbook)) return new Workbook();
+        this.SheetNames = [];
+        this.Sheets = {};
+    }
+
+    function s2ab(s) {
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+        return buf;
+    }
+
+
+    $scope.export_xlsx = function () {
+        var data = $scope.data;
+        if(data) {
+            var array_data = [];
+            array_data.push(['工号', '姓名', '性别', '专业技术职务', '最高学位', '学历', '所在学院', '主要学科', '主要学科导师', '第二学科', '第二学科导师', '手机号码', '电子邮箱', '备注']);
+            for(var i = 0; i < data.length; i ++) {
+                item = data[i];
+                array_data.push([item.id, item.xm, item.xb, item.zyjszw, item.zgxw, item.xl, item.szxy, item.zyxk, item.zyxk_ds, item.dexk, item.dexk_ds, item.sjhm, item.dzyx, item.bz]);
+            }
+
+            var ws_name = $scope.current_xy + "学科统计数据";
+            var wb = new Workbook();
+            var ws = sheet_from_array_of_arrays(array_data);
+
+            wb.SheetNames.push(ws_name);
+            wb.Sheets[ws_name] = ws;
+            var wbout = XLSX.write(wb, {bookType:'xlsx', bookSST:true, type: 'binary'});
+            saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), "data.xlsx")
+
+        }
+    }
+
     if(!$scope.szxys) {
         $http.get('/xktj/xy/')
             .success(function (data, status) {
